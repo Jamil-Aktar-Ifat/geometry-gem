0158703ed93fe91d2737491916ece6a42a4ea74c
ifat <i>
2024-04-15 17:49:32 +0100

    pentagon updated

--- scripts/pentagon-calc.js
+++ scripts/pentagon-calc.js
@@ -1,3 +1,23 @@
 function pentagonAreaCalc() {
-  console.log("Inside of Pentagon");
+  const perimeter = getInputValueByID("pentagon-perimeter");
+  //   console.log(perimeter);
+
+  const apo = getInputValueByID("pentagon-apo");
+  //   console.log(apo);
+
+  const area = 0.5 * apo * perimeter;
+
+  setInnerTextByID("pentagon-area", area);
+}
+
+function getInputValueByID(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputText = inputField.value;
+  const inputValue = parseFloat(inputText);
+  return inputValue;
+}
+
+function setInnerTextByID(elementId, area) {
+  const element = document.getElementById(elementId);
+  element.innerText = area;
 }
